d81f49b8c614f2499d944939f0d5f1877cb7806a
Kyungguk Min <min@kyungguk.com>
2025-08-13 09:14:20 +0900

    02: Import the updated code

--- 02-ascii_graph/ascii_graph_plot.py
+++ 02-ascii_graph/ascii_graph_plot.py
@@ -1,72 +1,71 @@
-from collections import deque
-from math import isclose, pi, sin
-
-# ____그래프를 그리기 위한 설정______________________
+from math import sin
 
 start = -10
 end = 10.3
 gap = 0.25
 
-count = (end - start) / gap
+x_i = [start + gap * i for i in range(int((end - start) / gap))]
 
-x_i = [start + gap * i for i in range(int(count))]
-print(x_i)
-
-
-y__ = [round(sin(xx), 1) for xx in x_i]
+y__ = [round(sin(k), 1) for k in x_i]
 y_k = list(set(y__))
 y_k.sort(reverse=True)
-print(y_k)
 
-# ____그래프_________________________________________
-# x축
+# 그래프 틀
+y_left = ["++", "+ "] + ["++", "| "] * 8 + ["++", "+ ", "++"]
+y_right = ["++", " +"] + ["++", " |"] * 8 + ["++", " +", "++"]
+ordinate1 = "".join(y_left)
+ordinate2 = "".join(y_right)
 
-x_ = ["+----------------"] * 4
-x_result = deque(x_)
-x_result.append("++")
-x_result.appendleft("+")
+# ' '으로 채워진 빈 이중 list
+graph = [[" " for width in range(len(x_i))] for length in range(len(y_k))]
 
-abscissa = "".join(x_result)
-print(abscissa)
+high = len(y_k)  # 중복되는 y값 제외한 값 개수: 21개(1.0 ~ -1.0)
+width = len(x_i)  # 출력되는 x값 개수: 81개
+y_max = 1.0
+y_min = -1.0
 
-
-# y축
-ordinate = ["++", "+"] + ["++", "|"] * 8 + ["++", "+", "++"]
-print(ordinate)
-print()
+xy_list = [[x, y] for x, y in zip(x_i, y__)]  # x와 y가 일대일 대응 됨.
 
 
-for index_y, y in enumerate(y_k):
-    if index_y % 2 == 0:
-        graph = "{:>4}{:>3}".format(round(y, 1), ordinate[index_y])
+# x값에 대한 인덱스 구하는 함수
+def index_x(x):
+    return int(abs(start - x) / gap)
+
+
+# y값에 대한 인덱스 구하는 함수
+def index_y(y):
+    return int(round((high - 1) / (y_max - y_min) * (y_max - y), 1))
+
+
+# y인덱스와 그에 맞는 x 인덱스 위치에 'x' 찍음: list 속 list
+for x, y in xy_list:
+    row = index_y(y)  # y축 index
+    col = index_x(x)  # x축 index
+    graph[row][0] = ordinate1[row * 2 : 2 + row * 2]
+
+    graph[0][col] = "-"
+    graph[20][col] = "-"
+
+    graph[0][80] = "++"
+    graph[20][80] = "++"
+
+    graph[row][80] = ordinate2[row * 2 : 2 + row * 2]
+    graph[row][col] = "x"
+    graph[1][64:77] = "sin(x) xxxxxx"
+
+
+for i in range(len(graph)):
+    if i % 2 == 0:
+        y_num = "{:>4.1f} ".format(y_k[i])
+        print(y_num, end="")
     else:
-        graph = "{:>6}{:>1}".format(ordinate[index_y], "")
+        print("     ", end="")
+    print("".join(graph[i]))
 
-    for index_x, x in enumerate(x_i):
-        x_about = round(sin(x), 1)
 
-        if isclose(x_about, y, abs_tol=1e-9):
-            graph += "*"
-
-        elif index_y == 0:
-            graph += "-"
-        elif index_y == len(y_k) - 1:
-            graph += "-"
-
-        else:
-            graph += " "
-
-    if index_y % 2 == 0:
-        graph += ordinate[index_y]
-    else:
-        graph += ordinate[index_y]
-
-    print(graph)
-
-# y축
-
-under_num = list(range(-5, 11, 5))
-print("{:>7.0f}".format(-10), end="")
-for h in under_num:
-    print("{:>21.0f}".format(h), end="")
+# x축 숫자
+print("      ", end="")
+for x, y in xy_list:
+    if index_x(x) % 20 == 0:  # index_x(x)%20==0 은 range(-10,11,5) 와 동일한 결과 출력
+        print("{:<20d}".format(int(x)), end="")
 print()
